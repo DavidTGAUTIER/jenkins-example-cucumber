f5fc6bb7adfd912bbbf0c73adbae6f73468190a5
DavidTGAUTIER <davidtiffeneau@live.fr>
2023-04-30 16:09:45 +0200

    add modify StepDefinitions.java

--- src/test/java/com/planetpope/cucumber/StepDefinitions.java
+++ src/test/java/com/planetpope/cucumber/StepDefinitions.java
@@ -7,5 +7,24 @@ import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions.*;
 
 public class StepDefinitions {
-
+    @Given("x={int}")
+    public void x(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("y={int}")
+    public void y(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("x and y are added")
+    public void x_and_y_are_added() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the value should be {int}")
+    public void the_value_should_be(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
